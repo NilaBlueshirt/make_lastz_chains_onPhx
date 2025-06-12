045d41cd991786844043d7eb080873f6b086f9ea
Nil Mu <47370677+NilaBlueshirt@users.noreply.github.com>
2025-06-12 10:41:23 -0700

    Update test.sh
    remove username and clean pathes.

--- test.sh
+++ test.sh
@@ -2,8 +2,8 @@
 #SBATCH --job-name=test_4
 #SBATCH --array=4
 #SBATCH -t 3-0
-#SBATCH --output=/scratch/tianche5/chains/log/WM_%A.%a.out
-#SBATCH --error=/scratch/tianche5/chains/log/WM_%A.%a.err
+#SBATCH --output=/scratch/username/chains/log/WM_%A.%a.out
+#SBATCH --error=/scratch/username/chains/log/WM_%A.%a.err
 #SBATCH --mem=50G
 #SBATCH -p public
 #SBATCH -q public
@@ -13,21 +13,21 @@ module load openjdk-17.0.3_7-4s
 module load mamba
 source activate make_lastz_chains-2.0.8_base
 
-ls /scratch/tianche5/chains/input
+ls /scratch/username/make_lastz_chains_onPhx/input
 
 # Path to the species list file
-species_list="/scratch/tianche5/chains/lastz_ref_query_list.txt"
+species_list="/scratch/username/make_lastz_chains_onPhx/lastz_ref_query_list.txt"
 
 # Extract reference and query species from the species list file
 species_pair=$(sed -n "${SLURM_ARRAY_TASK_ID}p" "$species_list")
 ref_species=$(echo "$species_pair" | cut -f1)
 query_species=$(echo "$species_pair" | cut -f2)
 
-genome_dir="/scratch/tianche5/chains/input"
-working_dir="/scratch/tianche5/chains/"
+genome_dir="/scratch/username/make_lastz_chains_onPhx/input"
+working_dir="/scratch/username/make_lastz_chains_onPhx/"
 export PATH=/packages/envs/make_lastz_chains-2.0.8_base/bin:$PATH
 
-cd /scratch/tianche5/chains/make_lastz_chains/
+cd /scratch/username/make_lastz_chains_onPhx/make_lastz_chains/
 
 ./make_chains.py -f --project_dir $working_dir/${ref_species}_${query_species}_4_5m \
 --cluster_executor slurm \
